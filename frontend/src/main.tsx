d50d7041548ab5f3d3d25d9349e3c2e4778c1959
Aliyu <biggy0023@gmail.com>
2023-08-16 22:56:51 +0100

    Setup react query

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,14 +7,14 @@ import {
   RouterProvider,
 } from "react-router-dom";
 
+import { HelmetProvider } from 'react-helmet-async';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App.tsx'
 import './index.css'
 import HomePage from './pages/HomePage.tsx';
 import ProductPage from './pages/ProductPage.tsx';
-import axios from 'axios';
-
-axios.defaults.baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : '/'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -26,8 +26,16 @@ const router = createBrowserRouter(
     </Route>
   )
 );
+
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <HelmetProvider>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </HelmetProvider>
   </React.StrictMode>,
 )
--- frontend/src/pages/ProductPage.tsx
+++ frontend/src/pages/ProductPage.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import { Helmet } from 'react-helmet-async'
 
 export default function ProductPage() {
-  return (
-    <div>ProductPage</div>
-  )
+  return <div>
+    <Helmet>
+      <title>Product Page</title>
+    </Helmet>
+    ProductPage</div>
 }
