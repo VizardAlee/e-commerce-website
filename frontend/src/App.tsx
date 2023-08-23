cb2c915a899410022ac3dd12b2ad7c4742172f87
Aliyu <biggy0023@gmail.com>
2023-08-23 17:15:55 +0100

    Succesfully created signin page and also the signout button works too

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { useContext, useEffect } from 'react'
-import { Badge, Button, Container, Nav, Navbar } from 'react-bootstrap'
+import { Badge, Button, Container, Nav, NavDropdown, Navbar } from 'react-bootstrap'
 import { Link, Outlet } from 'react-router-dom'
 import { LinkContainer } from 'react-router-bootstrap'
 import { ToastContainer } from 'react-toastify'
@@ -8,7 +8,7 @@ import { Store } from './Store'
 
 function App() {
   const {
-    state: { mode, cart },
+    state: { mode, cart, userInfo },
     dispatch,
   } = useContext(Store)
 
@@ -19,6 +19,16 @@ function App() {
   const switchModeHandler = () => {
     dispatch({ type:'SWITCH_MODE' })
   }
+
+  const signoutHandler = () => {
+    dispatch({ type: 'USER_SIGNOUT'})
+    localStorage.removeItem('userInfo')
+    localStorage.removeItem('cartItems')
+    localStorage.removeItem('shippingAddress')
+    localStorage.removeItem('paymentMethod')
+    window.location.href = '/signin'
+  }
+
   return (
     <div className="d-flex flex-column vh-100">
       <ToastContainer position="bottom-center" limit={1} />
@@ -41,7 +51,21 @@ function App() {
                 </Badge>
               )}
             </Link>
-            <a href="/signin" className='nav-link'>Sign In</a>
+            {userInfo ? (
+              <NavDropdown title={userInfo.name} id='basic-nav-dropdown'>
+                <Link
+                className='dropdown-item'
+                to="#signout"
+                onClick={signoutHandler}
+                >
+                  Sign Out
+                </Link>
+              </NavDropdown>
+            ) : (
+              <Link className='nav-link' to="/signin">
+                Sign In
+              </Link>
+            )}
           </Nav>
         </Navbar>
       </header>
--- frontend/src/Store.tsx
+++ frontend/src/Store.tsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import { Cart, CartItem } from './types/Cart'
+import { UserInfo } from './types/UserInfo'
 // import { start } from 'repl'
 
 type AppState = {
   mode: string
   cart: Cart
+
+  userInfo?: UserInfo
 }
 
 const initialState: AppState = {
+  userInfo: localStorage.getItem('userInfo')
+    ? JSON.parse(localStorage.getItem('userInfo')!)
+    : null,
+
   mode: localStorage.getItem('mode')
     ? localStorage.getItem('mode')!
     : window.matchMedia &&
@@ -35,6 +42,8 @@ type Action =
   | { type: 'SWITCH_MODE' }
   | { type: 'CART_ADD_ITEM'; payload: CartItem }
   | { type: 'CART_REMOVE_ITEM'; payload: CartItem }
+  | { type: 'USER_SIGNIN'; payload: UserInfo }
+  | { type: 'USER_SIGNOUT' }
 
 function reducer(state: AppState, action: Action): AppState {
   switch (action.type) {
@@ -63,7 +72,29 @@ function reducer(state: AppState, action: Action): AppState {
       localStorage.setItem('cartItems', JSON.stringify(cartItems))
       return { ...state, cart: { ...state.cart, cartItems } }
     }
-    
+
+    case 'USER_SIGNIN':
+      return { ...state, userInfo: action.payload }
+    case 'USER_SIGNOUT':
+      return {
+        ...state,
+        cart: {
+          cartItems: [],
+          paymentMethod: 'Paypal',
+          shippingAddress: {
+            fullName: '',
+            address: '',
+            postalCode: '',
+            city: '',
+            country: '',
+          },
+          itemsPrice: 0,
+          shippingPrice: 0,
+          taxPrice: 0,
+          totalPrice: 0,
+        }
+      }
+
     default:
       return state
   }
