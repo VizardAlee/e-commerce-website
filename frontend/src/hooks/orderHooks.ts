20efecfb622b87af003a1a4f7f90acad1168dc12
Aliyu <biggy0023@gmail.com>
2023-08-26 20:38:31 +0100

    Integrated PayPal API into the system

--- frontend/src/hooks/orderHooks.ts
+++ frontend/src/hooks/orderHooks.ts
@@ -9,6 +9,25 @@ useQuery({
   queryFn: async () => (await apiClient.get<Order>(`api/orders/${id}`)).data,
 })
 
+export const useGetPaypalClientIdQuery = () =>
+useQuery({
+  queryKey: ['paypal-clientId'],
+  queryFn: async () =>
+    (await apiClient.get<{ clientId: string }>(`/api/keys/paypal`))
+      .data,
+})
+
+export const usePayOrderMutation = () =>
+  useMutation({
+    mutationFn: async (details: { orderId: string }) =>
+      (
+        await apiClient.put<{message: string; order: Order}>(
+          `api/orders/${details.orderId}/pay`,
+          details
+        )
+      ).data,
+  })
+
 export const useCreateOrderMutation = () => useMutation({
   mutationFn:async (order: {
     orderItems: CartItem[]
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -24,6 +24,7 @@ import PaymentMethodPage from './pages/PaymentMethodPage.tsx';
 import ProtectedRoute from './components/ProtectedRoute.tsx';
 import PlaceOrderPage from './pages/PlaceOrderPage.tsx';
 import OrderPage from './pages/OrderPage.tsx';
+import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -51,12 +52,14 @@ const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <StoreProvider>
-      <HelmetProvider>
-        <QueryClientProvider client={queryClient}>
-          <RouterProvider router={router} />
-          <ReactQueryDevtools initialIsOpen={false} />
-        </QueryClientProvider>
-      </HelmetProvider>
+      <PayPalScriptProvider options={{ 'clientId': 'sb' }} deferLoading={true}>
+        <HelmetProvider>
+          <QueryClientProvider client={queryClient}>
+            <RouterProvider router={router} />
+            <ReactQueryDevtools initialIsOpen={false} />
+          </QueryClientProvider>
+        </HelmetProvider>
+      </PayPalScriptProvider>
     </StoreProvider>
   </React.StrictMode>,
 )
