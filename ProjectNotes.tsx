5725e5c272e715a60e43ae72d83dc2dd2badf400
Aliyu <biggy0023@gmail.com>
2023-10-15 11:08:32 +0100

    created a project notes page so as to keep track and also have directions  one what to get done next.

---
+++ ProjectNotes.tsx
@@ -0,0 +1,14 @@
+// things we wish to achieve with this platform
+
+// runnr is a unique e-commerce website which accomodates vendors and consumers
+// additionally, it also has a tracking  function where consumers can track their products directly from the app
+
+// the current position
+
+/*
+
+Using typescript, the basic function of making purchase order, adding item to cart and putting shipment 
+details and a test purchase order have been established. Now, the target is to commence development of the 
+pages that need working on so as first as how it should look before mechanizing it's functionality
+
+*/
